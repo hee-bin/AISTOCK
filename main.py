6581dcdb1666ddc9ce7fd9911748181824eadec5
hee-bin <gmlqls4732@naver.com>
2023-09-28 22:00:37 +0900

    ai poet donepractice done

---
+++ main.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+
+# from langchain.llms import OpenAI
+# llm = OpenAI()
+# re = llm.predict("how are you!")
+# print(re)
+
+from langchain.chat_models import ChatOpenAI
+chat_model = ChatOpenAI()
+
+st.title('welcome to make your own poem!')
+st.write('Hello, *World!* :sunglasses:')
+
+
+
+content = st.text_input('MAKING A COOOL POEM')
+
+if st.button('submit'):
+    with st.spinner('Wait for it...'):
+        result = chat_model.predict("write a poem about" + content )
+        st.write("subject is "+ content)
+        st.write(result)
