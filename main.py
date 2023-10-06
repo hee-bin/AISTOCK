0b205043fc465e72db884706e45fb3c1cf09a8ec
hee-bin <gmlqls4732@naver.com>
2023-10-06 10:58:23 +0900

    한국 번역으

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ content = st.text_input('It is supposed to be with a stock chart ')
 
 if st.button('I wanna a easier explain about the chart'):
     with st.spinner('Wait for it...'):
-        prompt = "You are an amazing financial analyst. As an analyst, analyze the stock chart information in detail and give me your brief opinion. here is the imformation"
+        prompt = "You are an amazing financial analyst. As an analyst, analyze the stock chart information in detail and give me your brief opinion in korean. here is the imformation"
         result = chat_model.predict(prompt + content )
-        st.write("subject is "+ content)
+        st.write(":pencil:"+ content)
         st.write(result)
